be0447eae4ec3a1ba17b205729a65e72e07eeee4
otherpirate <mauro_murari@hotmail.com>
2016-06-03 11:08:36 -0300

    Changing checker

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ browser.visit('http://google.com')
 browser.fill('q', 'splinter - python acceptance testing for web applications')
 browser.find_by_name('btnG').click()
 
-if browser.is_text_present('splinter.readthedocs.org'):
+if browser.is_text_present('splinter'):
     print "Yes, the official website was found!"
 else:
     print "No, it wasn't found... We need to improve our SEO techniques"
